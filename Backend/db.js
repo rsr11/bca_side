1fe23222a4983cccca4cad7609fb2af7ec22cea7
Rajeshwar <rsr45411@gmail.com>
2024-01-27 22:41:48 +0530

    second commit

--- Backend/db.js
+++ Backend/db.js
@@ -1,4 +1,16 @@
 // RqtI9bFHJ4Nn6IA0 - password
 // mongodb+srv://rockysingh800800:<password>@cluster0.1jezjbh.mongodb.net/  - link to connect the mongoDb
 
+const mongoose = require("mongoose");
 
+
+const connectingDataBase = async ()=>{
+           
+    try {
+        
+    } catch (error) {
+        console.log("error in connecting with database !!");
+    }
+
+
+}
